0ff65948ba8a1d6f4204795820e44773cd3137a2
codaubianca <codau_bianca@yahoo.com>
2024-01-31 08:47:53 +0000

    Added mage ai project

---
+++ week_2_workflow_orchestration/DataEngineeringZoomCamp2024/data_exporters/taxi_to_gcs_partitioned_parquet.py
@@ -0,0 +1,30 @@
+# not working
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/keys/positive-rhino-411414-837fae5602f4.json"
+bucket_name = 'mage-ai-ny-tdaxi-bucket'
+project_id = 'positive-rhino-411414'
+table_name = 'nyc_taxi_data_partitioned'
+
+root_path = f'{bucket_name}/{table_name}'
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    print("Exporting data...")
+    data['tpep_pickup_date'] = data['tpep_pickup_datetime'].dt.date
+    print("Converting to pandas")
+    table = pa.Table.from_pandas(data)
+    print("Grab gcs fs")
+    gcs = pa.fs.GcsFileSystem()
+    print("write to dataset")
+    pq.write_to_dataset(
+        table,
+        root_path=root_path,
+        partition_cols=['tpep_pickup_date'],
+        filesystem=gcs
+    )
---
+++ week_2_workflow_orchestration/Notes.md
@@ -0,0 +1,14 @@
+# Workflow Orchestration with Mage AI (Airflow alternative)
+
+IAM principal roles necessary for deployment with terraform
+- Owner 
+OR
+- Artifact Registry Reader
+- Artifact Registry Writer
+- Cloud Run Developer
+- Cloud SQL Admin
+- Service Account Token Creator
+
+Deploying to Google Cloud with Mage and Terraform
+- install gcloud cli and terraform
+- clone https://github.com/mage-ai/mage-ai-terraform-templates
